feccc56d4f5aeeba6f217fd51fd3d1dc33fb8b45
Matteo Muscas <thatman.j@gmail.com>
2018-11-21 16:04:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   if integer == 0 || integer == 1 || integer < 0
     return false
   else
-    (2..integer-1).to_a.all? do |number|
+    (2..integer).to_a.all? do |number|
       integer % number != 0
     end
   end
